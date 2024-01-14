72e4f13c433e99717077ef4f1b08167392e63286
Jaroslav Hošovský <j.hosovsky@seznam.cz>
2024-01-14 17:55:43 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import mobileDivider from "./img/pattern-divider-mobile.svg";
+import desktopDivider from "./img/pattern-divider-desktop.svg";
+import imgDice from "./img/icon-dice.svg";
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [advice, setAdvice] = useState("");
+  const [id, setId] = useState("");
+  const url = "https://api.adviceslip.com/advice";
+
+  const getAdvice = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const finalAdvice = data.slip.advice;
+    const id = data.slip.id;
+    setAdvice(finalAdvice);
+    setId(id);
+  };
+
+  const anotherAdvice = () => {
+    getAdvice();
+  };
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="card-wrapper">
+      <div className="text-wrapper">
+        <p>ADVICE # {id}</p>
+        <h1> "{advice}"</h1>
+        <img src={mobileDivider} alt="" className="divider" />
+        <div className="img-wrapper" onClick={anotherAdvice}>
+          <img src={imgDice} alt="" />
+        </div>
+      </div>
+    </section>
   );
-}
+};
 
 export default App;
+
+//
